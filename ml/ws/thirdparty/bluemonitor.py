7c55160260a120a1b4672cc09641c80b812530bf
dpatel-nec-labs <dpatel@nec-labs.com>
2023-05-15 13:55:18 -0400

    [thirdparty] Update API calls with the group name [setup] Fix invalid version no. retrieval

--- ml/ws/thirdparty/bluemonitor.py
+++ ml/ws/thirdparty/bluemonitor.py
@@ -15,7 +15,7 @@ class BlueMonitor(APIClient):
         assert self.secret, f'Cannot find API credentials for {self.api} at {SECRET_PATH}'
         self.auth_token = defaultdict(lambda: None)
 
-    def _auth_channel(self, channel_id, reload=False):
+    def _auth_channel(self, channel_id, group_name, reload=False):
         """
         curl -X GET "url" \
         -H "accept: application/json"  \
@@ -24,9 +24,12 @@ class BlueMonitor(APIClient):
         if self.auth_token[channel_id] is not None and not reload:
             pass
         else:
-            auth_creds = self.secret['auth']
+            auth_creds = self.secret['urls']['auth']
+            group_token = self.secret['group_tokens'].get(group_name, None)
+            assert group_token is not None, f'group_token not found for {group_name=}'
             url = auth_creds['url'].replace('channel_id', str(channel_id))
             headers = auth_creds['headers']
+            headers['Authorization'] = headers['Authorization'].replace('group_token', group_token)
             # request auth token
             res = self.get(url=url, headers=headers)
             if res.status_code == 200:
@@ -37,16 +40,17 @@ class BlueMonitor(APIClient):
 
         return self.auth_token[channel_id]
 
-    def stream_url(self, channel_id, stream_type='rtsp', reload=False):
+    def stream_url(self, channel_id, group_name, stream_type='rtsp', reload=False):
         """
         curl -X GET "url" \
         -H "accept: application/json" \
         -H "Authorization: Acc sdf15sd1f51sd4f1sd1f45sd1f"
         """
-        stream_creds = self.secret['stream']
-        ch_auth_token = self._auth_channel(channel_id, reload=reload)
+        stream_creds = self.secret['urls']['stream']
+        ch_auth_token = self._auth_channel(channel_id, group_name, reload=reload)
         headers = stream_creds['headers']
         url = stream_creds['url']
+        url = url.replace('channel_id', str(channel_id))
         headers['Authorization'] = headers['Authorization'].replace('auth_token', ch_auth_token)
         # request stream url
         res = self.get(url=url, headers=headers)
@@ -56,7 +60,7 @@ class BlueMonitor(APIClient):
         else:
             raise Exception(f'Failed to get the stream url {res.status_code}: {res.text}')
 
-    def send_meta(self, channel_id, data, reload=False):
+    def send_meta(self, channel_id, group_name, data, reload=False):
         """
         curl -X POST "url" 
         -H "accept: application/json" 
@@ -64,8 +68,8 @@ class BlueMonitor(APIClient):
         -H "Authorization: Acc sdf15sd1f51sd4f1sd1f45sd1f" 
         -d "json_data"
         """
-        meta_creds = self.secret['meta']
-        ch_auth_token = self._auth_channel(channel_id, reload=reload)
+        meta_creds = self.secret['urls']['meta']
+        ch_auth_token = self._auth_channel(channel_id, group_name, reload=reload)
         headers = meta_creds['headers']
         url = meta_creds['url']
         headers['Authorization'] = headers['Authorization'].replace('auth_token', ch_auth_token)
@@ -77,7 +81,7 @@ class BlueMonitor(APIClient):
         else:
             raise Exception(f'Failed to send meta {res.status_code}: {res.text}')
 
-    def send_event(self, channel_id, data, reload=False):
+    def send_event(self, channel_id, group_name, data, reload=False):
         """
         curl -X POST "url" \
         -H "accept: application/json" \
@@ -85,8 +89,8 @@ class BlueMonitor(APIClient):
         -H "Authorization: Acc adijiofenwenfiwenfiwnefwen"
         -d "json_data"
         """
-        event_creds = self.secret['event']
-        ch_auth_token = self._auth_channel(channel_id, reload=reload)
+        event_creds = self.secret['urls']['event']
+        ch_auth_token = self._auth_channel(channel_id, group_name, reload=reload)
         headers = event_creds['headers']
         url = event_creds['url']
         headers['Authorization'] = headers['Authorization'].replace('auth_token', ch_auth_token)
@@ -98,14 +102,14 @@ class BlueMonitor(APIClient):
         else:
             raise Exception(f'Failed to trigger event {res.status_code}: {res.text}')
     
-    def get_thumbnail(self, channel_id, start_ts, end_ts, reload=False):
+    def get_thumbnail(self, channel_id, group_name, start_ts, end_ts, reload=False):
         """
         curl -X GET "url" 
         -H "accept: application/json" 
         -H "Authorization: Acc adijiofenwenfiwenfiwnefwen"
         """
-        thumbnail_creds = self.secret['thumbnail']
-        ch_auth_token = self._auth_channel(channel_id, reload=reload)
+        thumbnail_creds = self.secret['urls']['thumbnail']
+        ch_auth_token = self._auth_channel(channel_id, group_name, reload=reload)
         headers = thumbnail_creds['headers']
         url = thumbnail_creds['url']
         headers['Authorization'] = headers['Authorization'].replace('auth_token', ch_auth_token)
